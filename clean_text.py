bd7ab0a1a8e0eda444be9124b1340f19a72cf9a3
Dave Hogue <thedavehogue@gmail.com>
2016-11-08 16:19:54 -0700

    finish custom stop words, pos tagging

--- clean_text.py
+++ clean_text.py
@@ -5,12 +5,14 @@ from multiprocessing import Pool, cpu_count
 from string import punctuation
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 STOPLIST = set(["n't", "'s", "'m", "ca", "'", "'re", "i've", 'poor', '-',
-                'worst', 'place', 'make', 'thing', 'hour', 'low', 'high',
-                'good', 'great', 'awesome', 'excellent', 'job', 'best', 'wonderful', 'awful',
-                'work', 'amazing', 'suck',
-                'bad', 'terrible', 'horrible', 'company', 'employee'] +
+                'worst', 'place', 'make', 'thing', 'hour', 'low', 'high', 'bos',
+                'good', 'great', 'awesome', 'excellent', 'job', 'best', 'lot',
+                'wonderful', 'awful', 'work', 'amazing', 'suck', 'nice',
+                'really', 'free', 'like', 'love', 'bad', 'terrible', 'care',
+                'horrible', 'company', 'employee', 'staff', 'time', 'day',
+                'week', 'month', 'year', 'need', 'better', 'just', 'decent'] +
                 list(ENGLISH_STOP_WORDS))
-KEEP_POS = {'ADJ','ADP','ADV','AUX','NOUN','VERB'}
+KEEP_POS = {'ADJ','ADP','ADV','NOUN','VERB'}
 nlp = spacy.load('en')
 
 
@@ -30,15 +32,15 @@ def clean_text(reviews):
 
 
 def lemmatize_text(text, stop_words=STOPLIST, keep_pos=KEEP_POS):
-    STOPLIST = set(["n't", "'s", "'m", "ca", "'", "'re", "i've", 'poor', '-',
-                    'worst', 'place', 'make', 'thing', 'hour', 'low', 'high',
-                    'good', 'great', 'awesome', 'excellent', 'job', 'best',
-                    'work', 'amazing', 'suck', 'decent', 'lot', 'time',
-                    'bad', 'terrible', 'horrible', 'company', 'employee'] +
-                    list(ENGLISH_STOP_WORDS))
-    KEEP_POS = {'ADJ','ADP','ADV','AUX','NOUN','VERB','X','PROPN'}
+    # STOPLIST = set(["n't", "'s", "'m", "ca", "'", "'re", "i've", 'poor', '-',
+    #                 'worst', 'place', 'make', 'thing', 'hour', 'low', 'high',
+    #                 'good', 'great', 'awesome', 'excellent', 'job', 'best',
+    #                 'work', 'amazing', 'suck', 'decent', 'lot', 'time',
+    #                 'bad', 'terrible', 'horrible', 'company', 'employee'] +
+    #                 list(ENGLISH_STOP_WORDS))
+    # KEEP_POS = {'ADJ','ADP','ADV','AUX','NOUN','VERB','X','PROPN'}
     x = nlp(text)
-    words = [tok.lemma_.strip(punctuation) for tok in x if (tok.pos_ in keep_pos) and (tok.lemma_ not in STOPLIST)]
+    words = [tok.lemma_.strip(punctuation) for tok in x if (tok.pos_ in keep_pos) and (tok.lemma_.strip(punctuation) not in STOPLIST)]
     words.extend(['boss' for tok in x if tok.lemma_ == 'bos'])
     return ' '.join(words)
 
